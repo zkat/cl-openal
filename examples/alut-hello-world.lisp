e7427408071ce36f7909217390744f4afb20a893
Marcus Eskilsson <marcus@sxemacs.org>
2009-07-29 11:41:00 +0200

    lispified

--- examples/alut-hello-world.lisp
+++ examples/alut-hello-world.lisp
@@ -10,15 +10,6 @@
 ;;;
 
 (defun alut-hello-world ()
-  (alut:init)
-  (let ((buffer (alut:create-buffer-hello-world))
-	(source (gen-source)))
-    (al:source source :buffer buffer)
-    (al:source-play source)
-    (alut:sleep 1)
-    (alut:exit)))
-
-(defun alut-hello-world ()
   (alut:with-init
     (al:with-source (source)
       (let ((buffer (alut:create-buffer-hello-world)))
